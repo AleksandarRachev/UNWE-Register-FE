2d74af0016baf4baedee21cd5f86fc2561235220
Aleksandar <rachev.96@abv.bg>
2020-03-22 19:55:19 +0200

    implemented edit profile

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -18,21 +18,48 @@ class ProfilePage extends React.Component {
     state = {
         data: null,
         file: null,
-        imagePreviewUrl: user.imageUrl
+        imagePreviewUrl: user.imageUrl,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        phone: user.phone,
+        address: user.address,
+        contactPerson: user.contactPerson
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        axios.put(GlobalVariables.backendUrl + "/users", this.state.data, { headers: headers })
+
+        const data = this.state.data == null ? new FormData() : this.state.data;
+        data.set('email', this.state.email === "" ? null : this.state.email);
+        data.set('firstName', this.state.firstName);
+        data.set('lastName', this.state.lastName);
+        data.set('phone', this.state.phone);
+        data.set('address', this.state.address);
+        data.set('contactPerson', this.state.contactPerson);
+        this.setState({ ...this.state, data: data });
+
+        axios.put(GlobalVariables.backendUrl + "/users", data, { headers: headers })
             .then(respone => {
+                user.email = this.state.email;
+                user.firstName = this.state.firstName;
+                user.lastName = this.state.lastName;
+                user.address = this.state.address;
+                user.contactPerson = this.state.contactPerson;
                 user.imageUrl = this.state.imagePreviewUrl
                 localStorage.setItem("user", JSON.stringify(user))
                 alert("Profile updated!")
             },
                 error => {
-
+                    this.setState({ ...this.state, error: null })
+                    if (error.response.data.message != null) {
+                        this.setState({ ...this.state, error: "There was an error: " + error.response.data.message })
+                    }
+                    else {
+                        this.setState({ ...this.state, error: "Oops there was a problem." })
+                    }
                 }
-            )
+            );
     }
 
     handleImageChange = (e) => {
@@ -50,14 +77,38 @@ class ProfilePage extends React.Component {
         reader.readAsDataURL(file)
 
         const data = this.state.data == null ? new FormData() : this.state.data;
-        data.append('image', file);
+        data.set('image', file);
         this.setState({ ...this.state, data: data });
     }
 
     handleEmailChange = (e) => {
-        const data = this.state.data == null ? new FormData() : this.state.data;
-        data.append('email', e.target.value);
-        this.setState({ ...this.state, data: data });
+        var email = e.target.value;
+        this.setState({ ...this.state, email: email === "null" ? null : email });
+    }
+
+    handleFirstNameChange = (e) => {
+        var firstName = e.target.value;
+        this.setState({ ...this.state, firstName: firstName === "null" ? null : firstName });
+    }
+
+    handleLasttNameChange = (e) => {
+        var lastName = e.target.value;
+        this.setState({ ...this.state, lastName: lastName === "null" ? null : lastName });
+    }
+
+    handlePhoneChange = (e) => {
+        var phone = e.target.value;
+        this.setState({ ...this.state, phone: phone === "null" ? null : phone });
+    }
+
+    handleAddressChange = (e) => {
+        var address = e.target.value;
+        this.setState({ ...this.state, address: address === "null" ? null : address });
+    }
+
+    handleContactPersonChange = (e) => {
+        var contactPerson = e.target.value;
+        this.setState({ ...this.state, contactPerson: contactPerson === "null" ? null : contactPerson });
     }
 
     render() {
@@ -65,21 +116,25 @@ class ProfilePage extends React.Component {
         if (this.state.imagePreviewUrl) {
             imagePreview = (<img className="profile-pic" src={this.state.imagePreviewUrl} />);
         } else {
-            imagePreview = (<div className="previewText">No profile picture</div>);
+            imagePreview = (<div className="preview-text">No profile picture</div>);
         }
 
         return (
-            <div className="previewComponent header">
-                <div className="imgPreview">{imagePreview}</div>
-                <form className="profile-edit" onSubmit={(e) => this.handleSubmit(e)}>
-                    <input className="fileInput" type="file" onChange={e => this.handleImageChange(e)} />
-                    <input value={user.email} placeholder="Email" onBlur={e => this.handleEmailChange(e)} />
-                    <input value={user.firstName} placeholder="First name" />
-                    <input value={user.lastName} placeholder="Last name" />
-                    <input value={user.address} placeholder="Address" />
-                    <input value={user.contactPerson} placeholder="Contact person" />
-                    <button className="submitButton" type="submit" onClick={e => this.handleSubmit(e)}>Upload Image</button>
-                </form>
+            <div className="header">
+                <div className="edit-form">
+                    {this.state.error && <Error message={this.state.error} />}
+                    <div className="img-preview">{imagePreview}</div>
+                    <form className="profile-edit" onSubmit={(e) => this.handleSubmit(e)}>
+                        <input className="file-input" type="file" onChange={e => this.handleImageChange(e)} />
+                        <input className="field-input" value={this.state.email} placeholder="* Email" onChange={e => this.handleEmailChange(e)} />
+                        <input className="field-input" value={this.state.firstName} placeholder="* First name" onChange={e => this.handleFirstNameChange(e)} />
+                        <input className="field-input" value={this.state.lastName} placeholder="* Last name" onChange={e => this.handleLasttNameChange(e)} />
+                        <input className="field-input" value={this.state.phone} placeholder="* Phone" onChange={e => this.handlePhoneChange(e)} />
+                        <input className="field-input" value={this.state.address} placeholder="Address" onChange={e => this.handleAddressChange(e)} />
+                        <input className="field-input" value={this.state.contactPerson} placeholder="Contact person" onChange={e => this.handleContactPersonChange(e)} />
+                        <button className="submit-edit-button" type="submit" onClick={e => this.handleSubmit(e)}>Edit profle</button>
+                    </form>
+                </div>
             </div>
         )
     }
